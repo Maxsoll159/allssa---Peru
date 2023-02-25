36016cf23c1ba7460c88715a07b910a3ae4de4f0
Martin <mar_1905@hotmail.es>
2023-02-24 22:58:46 -0500

    Cambio3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? 'https://dataverifica.com/' : undefined,
 }
 
 module.exports = nextConfig
