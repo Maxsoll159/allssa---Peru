74175a06437935b419867190061100c20b4812e7
Martin <mar_1905@hotmail.es>
2023-02-24 22:30:53 -0500

    dATA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? 'https://dataverifica.com/auth/signin' : undefined,
 }
 
 module.exports = nextConfig
