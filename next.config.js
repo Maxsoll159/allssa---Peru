b28cce1c7f47544ddbf5c4561bd3f227d3504d04
Martin <mar_1905@hotmail.es>
2023-02-27 19:41:55 -0500

    Quitado

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextBuildId = require('next-build-id')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  generateBuildId: () => nextBuildId({ dir: __dirname, describe: true })
+
 }
 
 module.exports = nextConfig
